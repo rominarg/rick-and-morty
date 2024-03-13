c99af9e3a08af0471947e3aa8234e5ba79d46be4
RominaNavarrete <rominagnavarrete@gmail.com>
2024-03-12 23:04:39 -0300

    creating components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import imageRickMorty from "./img/rick-morty.png";
+// import imageRickMorty from "./img/rick-morty.png";
+import imageRickMorty from "./img/rick-morty.webp";
 import "./App.css";
+
+// Importamos el hook useState desde React para manejar el estado de los personajes
 import { useState } from "react";
+
+// Importamos el componente Characters
 import Characters from "./components/Characters";
 
 function App() {
+  // Definimos el estado de los personajes usando el hook useState
   const [characters, setCharacters] = useState(null);
 
+  // Función para realizar la solicitud a la API y obtener los personajes
   const resApi = async () => {
+    // Realizamos la solicitud a la API
     const api = await fetch('https://rickandmortyapi.com/api/character');
 
+    // Convertimos la respuesta de la API a formato JSON
     const charactersApi = await api.json();
+
+    // Actualizamos el estado de los personajes con los datos obtenidos de la API
     setCharacters(charactersApi.results);
-  
-    // console.log(characters)
   }
+
+  // Renderizamos el componente App
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="title">Rick & Morty </h1>
+        {/* Título del proyecto */}
+        <h1 className="title">Rick & Morty</h1>
+
+        {/* Condicional para renderizar el componente Characters o el botón para buscar personajes */}
         {characters ? (
           <Characters characters={characters} setCharacters={setCharacters} />
         ) : (
           <>
+            {/* Imagen de Rick & Morty / Ternario*/}
             <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-            <button onClick={resApi} className="btn-search">
+
+            {/* Botón para buscar personajes */}
+            <button onClick={resApi} className="btn-search rounded-button">
               Buscar personajes
             </button>
           </>
